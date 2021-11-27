5eaaf72a60b221aa8a0e4b365b6445ef51723f9d
hoon park <hoonpark7442@gmail.com>
2021-11-27 16:21:20 +0900

    setup drawer api

--- tecnic-backend/lib/modules/read_mark_checker/readable_scopes.rb
+++ tecnic-backend/lib/modules/read_mark_checker/readable_scopes.rb
@@ -13,7 +13,6 @@ module ReadMarkChecker
 
       def unread_by(reader)
         result = join_read_marks(reader)
-
         if global_time_stamp = reader.read_mark_global(self).try(:timestamp)
           result.where("#{ReadMark.quoted_table_name}.id IS NULL
                         AND #{quoted_table_name}.#{connection.quote_column_name(readable_options[:on])} > ?", global_time_stamp)
